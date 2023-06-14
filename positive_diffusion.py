ada4132ea790c6c961e0b3f66df13b807bbcb208
Honghan Li <59818975+Minatsukiyoshino@users.noreply.github.com>
2023-06-14 11:22:17 +0300

    Update positive_diffusion.py

--- positive_diffusion.py
+++ positive_diffusion.py
@@ -42,21 +42,16 @@ def sigmoid_beta_schedule(timesteps):
 
 
 timesteps = params.timesteps
-
-# 定义方差schedule
 betas = cosine_beta_schedule(timesteps=timesteps)
 
-# define alphas
 alphas = 1. - betas
 alphas_cumprod = torch.cumprod(alphas, axis=0)
 alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
 sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
 
-# calculations for diffusion q(x_t | x_{t-1}) and others
 sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
 sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
 
-# calculations for posterior q(x_{t-1} | x_t, x_0)
 posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
 
 
